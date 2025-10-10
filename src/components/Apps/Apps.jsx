f5e565ef0f04996fe2cafb321a4d5403c9d23ee0
Burhan <burhan.umt@gmail.com>
2025-10-10 10:53:31 +0300

    added serch value not match error

--- src/components/Apps/Apps.jsx
+++ src/components/Apps/Apps.jsx
@@ -7,24 +7,32 @@ import Spinner from "../Spinner/Spinner";
 const Apps = () => {
   const { appsData, loading, error } = useApps();
   const [inputValue, setInputValue] = useState("");
-  const [searchLoading, setSearchLoading] = useState(false)
+  const [searchLoading, setSearchLoading] = useState(false);
   const inputValueTrim = inputValue.trim().toLowerCase();
-  const searchValueData = inputValueTrim ? appsData.filter(app => app.title.toLowerCase().includes(inputValueTrim)) : appsData;
-  useEffect(()=>{
-    if(inputValue === "") return;
+  const searchValueData = inputValueTrim
+    ? appsData.filter((app) => app.title.toLowerCase().includes(inputValueTrim))
+    : appsData;
+  useEffect(() => {
+    if (inputValue === "") return;
     setSearchLoading(true);
     const timer = setTimeout(() => {
-      setSearchLoading(false)
+      setSearchLoading(false);
     }, 300);
-    return ()=> clearTimeout(timer)
-  }, [inputValue])
-  if(!searchValueData.length) return <AppsNotFound/>
+    return () => clearTimeout(timer);
+  }, [inputValue]);
+  
   return (
     <div>
-        <h1 className="text-3xl md:text-5xl lg:text-5xl font-bold text-[#001931] text-center mb-5">Our All Applications</h1>
-        <p className="text-[#627382] text-xl text-center mb-5">Explore All Apps on the Market developed by us. We code for Millions</p>
+      <h1 className="text-3xl md:text-5xl lg:text-5xl font-bold text-[#001931] text-center mb-5">
+        Our All Applications
+      </h1>
+      <p className="text-[#627382] text-xl text-center mb-5">
+        Explore All Apps on the Market developed by us. We code for Millions
+      </p>
       <div className="flex justify-between items-center p-3">
-        <h1 className="font-semibold text-2xl">({searchValueData.length}) Apps Found</h1>
+        <h1 className="font-semibold text-2xl">
+          ({searchValueData.length}) Apps Found
+        </h1>
         <div>
           <label className="input">
             <svg
@@ -43,23 +51,25 @@ const Apps = () => {
                 <path d="m21 21-4.3-4.3"></path>
               </g>
             </svg>
-            <input onChange={(e)=> setInputValue(e.target.value)} type="search" required placeholder="Search Apps" />
+            <input
+              onChange={(e) => setInputValue(e.target.value)}
+              type="search"
+              required
+              placeholder="Search Apps"
+            />
           </label>
         </div>
       </div>
       <div className="flex justify-center items-center">
-          {
-          searchLoading && <Spinner/>
-
-        }
-        </div>
+        {searchLoading && <Spinner />}
+      </div>
+      <div>
+        {!searchValueData.length && <AppsNotFound/>}
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 p-3">
-        {
-            searchValueData.map(singleApp => <DisplayApps
-            key={singleApp.id}
-            singleApp={singleApp}
-            />)
-        }
+        {searchValueData.map((singleApp) => (
+          <DisplayApps key={singleApp.id} singleApp={singleApp} />
+        ))}
       </div>
     </div>
   );
--- src/components/AppsNotFound/AppsNotFound.jsx
+++ src/components/AppsNotFound/AppsNotFound.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router";
-
+import appErrorImg from "../../assets/App-Error.png"
 const AppsNotFound = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="text-7xl text-[#627382] my-7">OPPS! 404 No Apps Found</h1>
+    <div className="flex flex-col justify-center items-center text-center">
+      <img className="w-50" src={appErrorImg} alt="" />
+      <h1 className="text-4xl text-[#627382] my-7">OPPS! 404 No Apps Found</h1>
       <Link to="/">
-        <button className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] py-2 px-10 rounded-sm cursor-pointer text-white font-semibold my-5">
+        <button className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] py-2 px-10 rounded-sm cursor-pointer text-white font-semibold ">
           Show All
         </button>
       </Link>
